889520ee196c739596acb449f16db4762db2a770
GrishaYak <gishayak2@gmail.com>
2026-04-08 18:44:41 +0500

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# LeetCode Solving
+_**Here I solve tasks from LeetCode**_
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "string"
+#include "vector"
+#include "iostream"
+
+using namespace std;
+typedef long long ll;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
+
+int main() {
+    cout << 5 + 3 << endl;
+}
