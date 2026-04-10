26b805e98d78628529c28361b94f4847a7a76715
GrishaYak <gishayak2@gmail.com>
2026-04-10 09:38:40 +0500

    Add Two Numbers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,90 @@
-#include "string"
-#include "vector"
 #include "iostream"
-
+#include "vector"
+#include "algorithm"
 using namespace std;
-typedef long long ll;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* res = new ListNode();
+        bool transfer = false;
+        ListNode* cur = res;
+        while (true) {
+            int val1 = 0, val2= 0;
+            if (l1 != nullptr) {
+                val1 = l1->val;
+            }
+            if (l2 != nullptr) {
+                val2 = l2->val;
+            }
+            int summ = val1 + val2 + transfer;
+            transfer = false;
+            if (summ > 9) {
+                transfer = true;
+                summ -= 10;
+            }
+            cur->val = summ;
+            if (l1->next == nullptr && l2->next == nullptr) {
+                break;
+            }
+            cur->next = new ListNode();
+            cur = cur->next;
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        return res;
     }
 };
 
+ListNode* from_vector(vector<int>& input, int index) {
+    if (index >= input.size() - 1) {
+        return new ListNode(input[index]);
+    }
+    ListNode* res = new ListNode(input[index], from_vector(input, index + 1));
+    return res;
+}
+
+ListNode* from_vector(vector<int> input) {
+    return from_vector(input, 0);
+}
+
+vector<int> to_vector(ListNode* l1) {
+    auto my_copy = l1;
+    vector<int> res;
+    while(my_copy != nullptr) {
+        res.push_back(my_copy->val);
+        my_copy = my_copy->next;
+    }
+    return res;
+}
+
+void print_vector(vector<int>& arr) {
+    for (int el : arr) {
+        cout << el << ' ';
+    }
+    cout << '\n';
+}
+
+void print_list(ListNode* l) {
+    while (l != nullptr)
+    {
+        cout << l->val << ' ';
+        l = l->next;
+    }
+    cout << '\n';
+    
+}
+
 int main() {
-    cout << 5 + 3 << endl;
+    ListNode* l1 = from_vector({2, 4, 3});
+    ListNode* l2 = from_vector({5, 6, 4});
+    ListNode* summ = Solution().addTwoNumbers(l1, l2);
+    print_list(summ);
 }
